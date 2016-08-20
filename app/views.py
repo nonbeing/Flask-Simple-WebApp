0b6709d13aca810c8d99083437545825287189f0
Ambar Seksena <anattaspace@gmail.com>
2016-08-20 19:18:17 +0530

    Various improvements
    - add helper method to generate _hmac_256()
    - generate unique sig based on secret_key + unix ts as 'state' param for slack button

--- app/views.py
+++ app/views.py
@@ -1,27 +1,44 @@
 from flask import render_template
 from app import app
+import hashlib
+import hmac
+import time
+import os
+
+CLIENT_ID = "20006151314.70868885203"
+
+with open(os.path.join(os.path.dirname(__file__), 'HMAC_SECRET_KEY')) as f:
+    _HMAC_KEY = f.read().strip()
+
+def _hmac_sha256(message, key=_HMAC_KEY):
+    # Generate the hash.
+    signature = hmac.new(
+        key,
+        str(message),
+        hashlib.sha256
+    ).hexdigest()
+
+    return signature
+
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Ambar'}
-    posts = [
-        {
-            'author': {'nickname': 'John'},
-            'body': 'Beautiful day in Pune!'
-        },
-        {
-            'author': {'nickname': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
+    now_ts = time.time()
+    signature = _hmac_sha256(now_ts)
+
+    # save the signature for later
+    # TODO: needs to go into a db, even sqlite will do
+    with open('/tmp/sig', 'a') as f:
+        f.write("{}\n".format(signature))
+
     return render_template("index.html",
-                           title='Home',
-                           user=user,
-                           posts=posts)
+        client_id=CLIENT_ID,
+        title='Home',
+        signature=signature)
 
 
 @app.route('/oauth')
 def oauth():
-
+    pass
