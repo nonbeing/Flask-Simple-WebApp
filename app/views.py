9b5b3c8bf03df51ae781cb4018a40dde090d623c
Ambar Seksena <anattspace@gmail.com>
2016-08-20 19:48:12 +0000

    working on slack.oauth.access

--- app/views.py
+++ app/views.py
@@ -1,11 +1,20 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 import hashlib
 import hmac
 import time
 import os
+from slacker import Slacker
+
 
 CLIENT_ID = "20006151314.70868885203"
+CLIENT_SECRET = "07a40f40126b4ce167c36c39e80411f1"
+SIG_FILE_PATH = "./tmp/sig"
+REDIRECT_URI = "http://nonbeing.tech/oauth"
+
+with open(os.path.join(os.path.dirname(__file__), 'SLACK_BOT_API_TOKEN')) as f:
+    BOT_API_TOKEN = f.read().strip()
+slack = Slacker(BOT_API_TOKEN)
 
 with open(os.path.join(os.path.dirname(__file__), 'HMAC_SECRET_KEY')) as f:
     _HMAC_KEY = f.read().strip()
@@ -30,15 +39,20 @@ def index():
 
     # save the signature for later
     # TODO: needs to go into a db, even sqlite will do
-    with open('/tmp/sig', 'a') as f:
+    with open(SIG_FILE_PATH, 'a') as f:
         f.write("{}\n".format(signature))
 
     return render_template("index.html",
         client_id=CLIENT_ID,
         title='Home',
-        signature=signature)
+        signature=signature,
+        redirect_uri=REDIRECT_URI)
 
 
 @app.route('/oauth')
 def oauth():
+    code = request.args.get('code')
+    response = slack.oauth.access(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, code=code)
     pass
+
+
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 #!flask/bin/python
 from app import app
-#app.run(debug=True)
-app.run(host='0.0.0.0', port=80)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
