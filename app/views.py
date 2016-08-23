9c0ed59128ec7a15cb4a654bae5364f8306c1225
nonbeing <anattaspace@gmail.com>
2016-08-24 02:10:35 +0530

    DynamoDB. Various other improvements:
    - Storing Slack team-specific creds in DynamoDB now
    - Removed 'incoming_webhooks' from the Slack button as this scope is unnecessary
    - Externalized all secrets to a separate ConfigParser file: 'config/SECRETS.ini'
    - Added HTML templates for error and success
    - Returning success message on Oauth Success

--- app/views.py
+++ app/views.py
@@ -5,38 +5,40 @@ import hmac
 import time
 import os
 from slacker import Slacker
+import json
 import logging
+import boto3
+import ConfigParser
+
+config = ConfigParser.ConfigParser()
+config.read('config/SECRETS.ini')
 
 
-CLIENT_ID = "20006151314.70868885203"
-CLIENT_SECRET = "07a40f40126b4ce167c36c39e80411f1"
-
-SIG_FILE_PATH = "./tmp/sig"
-REDIRECT_URI = "https://nonbeing.tech/oauth"
-
-with open(os.path.join(os.path.dirname(__file__), 'SLACK_BOT_API_TOKEN')) as f:
-    BOT_API_TOKEN = f.read().strip()
-slack = Slacker(BOT_API_TOKEN)
-
-with open(os.path.join(os.path.dirname(__file__), 'HMAC_SECRET_KEY')) as f:
-    _HMAC_KEY = f.read().strip()
+# Read global consts from config
+SLACK_CLIENT_ID = config.get('slack', 'SLACK_CLIENT_ID')
+SLACK_CLIENT_SECRET = config.get('slack', 'SLACK_CLIENT_SECRET')
+SLACK_BOT_API_TOKEN = config.get('slack', 'SLACK_BOT_API_TOKEN')
+APP_DYNAMODB_TABLE = config.get('webapp', 'APP_DYNAMODB_TABLE')
+APP_HMAC_KEY = config.get('webapp', 'APP_HMAC_KEY')
+APP_REDIRECT_URI = config.get('webapp', 'APP_REDIRECT_URI')
+APP_SIG_FILE_PATH = config.get('webapp', 'APP_SIG_FILE_PATH')
 
 
-# create logger with 'spam_application'
+slack = Slacker(SLACK_BOT_API_TOKEN)
+
+# create file logger
 logger = logging.getLogger('flask_webapp')
 logger.setLevel(logging.DEBUG)
-# create file handler which logs even debug messages
 fh = logging.FileHandler("/tmp/flaskwebapp.log")
 fh.setLevel(logging.DEBUG)
-
 formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
 fh.setFormatter(formatter)
 logger.addHandler(fh)
 
 
 
-def _hmac_sha256(message, key=_HMAC_KEY):
-    # Generate the hash.
+def _hmac_sha256(message, key=APP_HMAC_KEY):
+    # Generate the hmac-sha256 hash
     signature = hmac.new(
         key,
         str(message),
@@ -48,6 +50,20 @@ def _hmac_sha256(message, key=_HMAC_KEY):
 
 
 
+def _add_dynamodb_item(item, dynamodb_table_name):
+    session = boto3.Session(region_name=AWS_REGION)
+    dynamodb = session.resource('dynamodb')
+
+    table = dynamodb.Table(dynamodb_table_name)
+
+    dynamodb_response = table.put_item(
+       Item=item
+    )
+
+    logger.info("DynamoDB PutItem Response:\n{}".format(json.dumps(dynamodb_response, indent=4)))
+
+
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -56,45 +72,52 @@ def index():
 
     logger.info("now_ts: '{}'\nsignature: '{}'".format(now_ts, signature))
 
-
     # save the signature for later
     # TODO: needs to go into a db, even sqlite will do
-    with open(SIG_FILE_PATH, 'a') as f:
+    with open(APP_SIG_FILE_PATH, 'a') as f:
         f.write("{}\n".format(signature))
+
+    #TODO: Instead of adding the "Add to Slack" button, have a "Sign into Slack" link here
+    # When the user has signed into his team, we can take a hash of the team name instead of an
+    # arbitrary, useless hash
 
     return render_template("index.html",
         client_id=CLIENT_ID,
         title='Home',
         signature=signature,
-        redirect_uri=REDIRECT_URI)
+        redirect_uri=APP_REDIRECT_URI)
 
 
 
 @app.route('/oauth')
 def slack_oauth():
+    # grab the 'code' and 'state' from the incoming Slack request
     code = request.args.get('code')
-    logger.info("oauth - code: '{}'".format(code))
+    state = request.args.get('state')
+
+    # TODO: Verify that state is the same as was sent to us initially
+    # STATE VERIFICATION STEPS
+    logger.info("oauth - code: '{}', oauth - state: '{}'".format(code, state))
 
     try:
         if code:
-            response = slack.oauth.access(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, code=code)
-            logger.info("Slack Oauth response for code='{}' =\n{}".format(code, response))
+            slack_response = slack.oauth.access(client_id=SLACK_CLIENT_ID, client_secret=SLACK_CLIENT_SECRET, code=code)
+            logger.info("Slack Oauth response for code='{}' = {}".format(code, slack_response))
+            oauth_json = json.loads(slack_response)
+
+            if oauth_json['ok']:
+                dynamo_item = { 'team_id': oauth_json['team_id'], 'team_name': oauth_json['team_name'], 'access_token': oauth_json['access_token'], 'scope': oauth_json['scope'], 'user_id': oauth_json['user_id'], 'bot_user_id': oauth_json['bot']['bot_user_id'], 'bot_access_token': oauth_json['bot']['bot_access_token'], 'ok': oauth_json['ok'] }
+
+                _add_dynamodb_item(dynamo_item, APP_DYNAMODB_TABLE)
+            else:
+                return render_template("error.html", error_type="Oauth", description="We're sorry, but your Slack Authorization Flow somehow failed", details="{}".format(slack_response))
+
         else:
             logger.error("Incoming request to /oauth was missing the expected 'code' param ")
-            return "ERROR: no code?!"
+            return render_template("error.html", error_type="Oauth", description="We're sorry, but your Slack Authorization Flow failed because of a missing 'code' param from Slack.", details="{}".format(slack_response))
     except Exception as e:
-        logger.error("General Exception: `{}`".format(str(e)))
-    return response
+        logger.error("General Exception: '{}'".format(str(e)))
 
-
-# ZeroSSL challenges:
-# @app.route('/.well-known/acme-challenge/mdaDswXLn73yN8Zv8Kfoxoa8R7k2Lg7fgAhobwne1Ig')
-# def challenge1():
-#     return "mdaDswXLn73yN8Zv8Kfoxoa8R7k2Lg7fgAhobwne1Ig.LwhNiXeAtyFQtGtYKfpys8KYzU4VM2XBtVVVk37d6Kw"
-
-
-# @app.route('/.well-known/acme-challenge/NP1ijvAqlJArbOwWazhfnSs9PPcioYcn7dq_Jzsk9K4')
-# def challenge2():
-#     return "NP1ijvAqlJArbOwWazhfnSs9PPcioYcn7dq_Jzsk9K4.LwhNiXeAtyFQtGtYKfpys8KYzU4VM2XBtVVVk37d6Kw"
-
+    # all went well, return success message
+    return render_template("success.html", description="Thank you for adding OpsBot to your Slack team!")
 
